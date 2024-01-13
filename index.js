0166da942d84f757ac70d1f4791692f5a9c87502
Vanessa D Barbosa <van_dbarbosa@hotmail.com>
2024-01-13 12:10:13 -0300

    projeto concluido com sucesso

---
+++ index.js
@@ -0,0 +1,43 @@
+
+let saldovitoriasTotal= saldoVitorias (115, 14 ) 
+
+function saldoVitorias (vitorias, derrotas) {
+
+    let quantidadevitorias= vitorias
+    let vitoriasTotal= vitorias-derrotas
+    let nameHeroi= "Jacolle"
+ 
+    
+    console.log("O herói " + nameHeroi + " tem saldo de " + vitoriasTotal + " e está no " )
+
+
+    if (quantidadevitorias <10) { console.log ( "nivel Ferro" )
+            } 
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            else if (vitoriasTotal >=11 & vitoriasTotal <=20) { return nivel= "Bronze"
+            } 
+
+    else if (quantidadevitorias >=21 && quantidadevitorias<=50) { console.log ("nivel Prata" )              
+            }
+
+    else if (quantidadevitorias >=51 && quantidadevitorias <=80) { console.log ("nivel Ouro")
+            }
+
+    else if (quantidadevitorias >=81 && quantidadevitorias <=90) { console.log ( "nivel Diamante")
+            }
+
+    else if (quantidadevitorias >=91 && quantidadevitorias <=100) { console.log  ("nivel Lendário") 
+                
+            }
+
+    else if (quantidadevitorias >=101) { console.log  (nivel= "nivel Imortal")}
+
+    return vitoriasTotal          
+            
+
+                       
+           
+
+     }
+            
+    
